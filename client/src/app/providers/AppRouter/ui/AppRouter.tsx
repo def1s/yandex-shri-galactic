853443f23e943dfe696c45a985edea0f52ed9d60
Daniil <temnay2@icloud.com>
2025-06-22 15:58:23 +0300

    update: README

--- client/src/app/providers/AppRouter/ui/AppRouter.tsx
+++ client/src/app/providers/AppRouter/ui/AppRouter.tsx
@@ -1,16 +1,21 @@
 import { Suspense } from 'react';
 import { useCallback } from 'react';
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import { routerConfig } from '../routerConfig/routerConfig';
 import type { AppRoutesProps } from '../routerConfig/routerConfig';
 
 export const AppRouter = () => {
 	const renderWithWrapper = useCallback((route: AppRoutesProps) => {
-		// TODO: добавить Loader
 		const element = <Suspense fallback={'Loading...'}>{route.element}</Suspense>;
 
 		return <Route key={route.path} path={route.path} element={element} />;
 	}, []);
 
-	return <Routes>{Object.values(routerConfig).map(renderWithWrapper)}</Routes>;
+	return (
+		<Routes>
+			<Route path="/" element={<Navigate to="/analytics" replace />} />
+
+			{Object.values(routerConfig).map(renderWithWrapper)}
+		</Routes>
+	);
 };
